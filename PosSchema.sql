cbd30f8bb51d6970a7d1f4a147876a44473c379c
Daniel Flores <danielflores125@gmail.com>
2020-10-27 10:47:21 -0700

    Edited paths for t2 micros

--- PosSchema.sql
+++ PosSchema.sql
@@ -4,7 +4,8 @@ CREATE DATABASE bookings_service100;
 
 \c bookings_service100;
 
-CREATE TABLE restaraunts (RIMARY KEY,
+CREATE TABLE restaraunts (
+  restaraunt_id SERIAL PRIMARY KEY,
   restaraunt_name VARCHAR(50) NOT  NULL,
   restaraunt_address VARCHAR(50) NOT NULL,
   food_type VARCHAR(30) NOT NULL
@@ -25,41 +26,41 @@ CREATE TABLE reservation_times (
   booking_date DATE NOT NULL,RENCES restaraun_id INT REFERENCES users(user_id)
 );
 
-COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/reservationInfo1.csv' DELIMITER ',' CSV HEADER;
-COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/restaurantInfo1.csv' DELIMITER ',' CSV HEADER;
-COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/restaurantInfo2.csv' DELIMITER ',' CSV HEADER;
-COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/restaurantInfo3.csv' DELIMITER ',' CSV HEADER;
-COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/restaurantInfo4.csv' DELIMITER ',' CSV HEADER;
-COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/restaurantInfo5.csv' DELIMITER ',' CSV HEADER;
-COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/restaurantInfo6.csv' DELIMITER ',' CSV HEADER;
+COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/home/ubuntu
+reservationInfo1.csv' DELIMITER ',' CSV HEADER;
+COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/home/ubuntu/restaurantInfo2.csv' DELIMITER ',' CSV HEADER;
+COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/home/ubuntu/restaurantInfo3.csv' DELIMITER ',' CSV HEADER;
+COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/home/ubuntu/restaurantInfo4.csv' DELIMITER ',' CSV HEADER;
+COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/home/ubuntu/restaurantInfo5.csv' DELIMITER ',' CSV HEADER;
+COPY restaraunts (restaraunt_name, restaraunt_address, food_type) FROM '/home/ubuntu/restaurantInfo6.csv' DELIMITER ',' CSV HEADER;
 
-COPY users (first_name, last_name, phone_number, email, party_size) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/userInfo1.csv' DELIMITER ',' CSV HEADER;
-COPY users (first_name, last_name, phone_number, email, party_size) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/userInfo2.csv' DELIMITER ',' CSV HEADER;
-COPY users (first_name, last_name, phone_number, email, party_size) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/userInfo3.csv' DELIMITER ',' CSV HEADER;
-COPY users (first_name, last_name, phone_number, email, party_size) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/userInfo4.csv' DELIMITER ',' CSV HEADER;
-COPY users (first_name, last_name, phone_number, email, party_size) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/userInfo5.csv' DELIMITER ',' CSV HEADER;
-COPY users (first_name, last_name, phone_number, email, party_size) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/userInfo6.csv' DELIMITER ',' CSV HEADER;
-COPY users (first_name, last_name, phone_number, email, party_size) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/userInfo7.csv' DELIMITER ',' CSV HEADER;
-COPY users (first_name, last_name, phone_number, email, party_size) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/userInfo9.csv' DELIMITER ',' CSV HEADER;
-COPY users (first_name, last_name, phone_number, email, party_size) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/userInfo10.csv' DELIMITER ',' CSV HEADER;
-COPY users (first_name, last_name, phone_number, email, party_size) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/userInfo11.csv' DELIMITER ',' CSV HEADER;
+COPY users (first_name, last_name, phone_number, email, party_size) FROM '/home/ubuntu/userInfo1.csv' DELIMITER ',' CSV HEADER;
+COPY users (first_name, last_name, phone_number, email, party_size) FROM '/home/ubuntu/userInfo2.csv' DELIMITER ',' CSV HEADER;
+COPY users (first_name, last_name, phone_number, email, party_size) FROM '/home/ubuntu/userInfo3.csv' DELIMITER ',' CSV HEADER;
+COPY users (first_name, last_name, phone_number, email, party_size) FROM '/home/ubuntu/userInfo4.csv' DELIMITER ',' CSV HEADER;
+COPY users (first_name, last_name, phone_number, email, party_size) FROM '/home/ubuntu/userInfo5.csv' DELIMITER ',' CSV HEADER;
+COPY users (first_name, last_name, phone_number, email, party_size) FROM '/home/ubuntu/userInfo6.csv' DELIMITER ',' CSV HEADER;
+COPY users (first_name, last_name, phone_number, email, party_size) FROM '/home/ubuntu/userInfo7.csv' DELIMITER ',' CSV HEADER;
+COPY users (first_name, last_name, phone_number, email, party_size) FROM '/home/ubuntu/userInfo9.csv' DELIMITER ',' CSV HEADER;
+COPY users (first_name, last_name, phone_number, email, party_size) FROM '/home/ubuntu/userInfo10.csv' DELIMITER ',' CSV HEADER;
+COPY users (first_name, last_name, phone_number, email, party_size) FROM '/home/ubuntu/userInfo11.csv' DELIMITER ',' CSV HEADER;
 
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo1.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo2.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo3.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo4.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo5.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo6.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo7.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo8.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo9.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo10.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo11.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo12.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo13.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo14.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo15.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo16.csv' DELIMITER ',' CSV HEADER;
-COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/reservationInfo17.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo1.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo2.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo3.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo4.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo5.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo6.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo7.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo8.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo9.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo10.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo11.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo12.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo13.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo14.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo15.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo16.csv' DELIMITER ',' CSV HEADER;
+COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/reservationInfo17.csv' DELIMITER ',' CSV HEADER;
 
--- COPY reservation_times (time_slot, booking_date) FROM '/Users/danielflores/Desktop/HR130/Top-Table/bookings-service/database/seeding/posGres/foreignKey.csv' DELIMITER ',' CSV HEADER;
+-- COPY reservation_times (time_slot, booking_date) FROM '/home/ubuntu/foreignKey.csv' DELIMITER ',' CSV HEADER;
